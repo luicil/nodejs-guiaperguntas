faabb200316540b06fb3cbd5e213daf469d85d6a
Luicil Fernandes <luicilfernandes@hotmail.com>
2021-03-30 20:15:19 -0300

    bootstrap

--- index.js
+++ index.js
@@ -2,31 +2,14 @@ const express = require("express");
 const app = express();
 const port = 80;
 
-app.set("view engine","ejs");
-app.use(express.static("public"));
-app.get("/:nome?/:lang?",(req, res) =>{
-    //res.send("Bem vindo ao perguntas e respostas.")
-
-    var nome = req.params.nome ? req.params.nome : "Luicil";
-    var lang = req.params.lang ? req.params.lang : "VB";
-    var produtos=[
-        {nome: "banana", preco: 5.5},
-        {nome: "cebola", preco: 2.2}
-    ]
-    const exbMsg = false;
-    //res.render("index");
-    res.render("index",{
-        nome: nome,
-        lang: lang,
-        Msg: exbMsg,
-        produtos
-    });
-    //res.render("home");
-    //res.render("principal/usuario");
+app.set("view engine", "ejs");
+app.use(express.static("public"))
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
-app.listen(port,(erro) =>{
-    if(erro){
+app.listen(port, (erro) => {
+    if (erro) {
         console.log("Erro ao iniciar o servidor.");
     } else {
         console.log("Servidor iniciado na porta: " + port);
